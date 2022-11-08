449d522602564791a670dfb5e19dd56093d9e0e2
joaopgmiranda <joaomiranda.ufjf@gmail.com>
2022-11-07 21:32:33 -0300

    Componente Upload Imagem

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
 import Avatar from '../components/avatar'
 import Botao from '../components/botao'
 import { UploadImagem } from '../components/uploadImagem'
 
 export default function Home() {
+  const [imagem, setImagem] = useState(null);
+  const referenciaInput = useRef(null);
+  
+  console.log(imagem);
+  
   return (
     <>
       <h1>Ola mundo!</h1>
-      <UploadImagem />
+      <button onClick={() => referenciaInput?.current?.click()}>abrir seletor de arquivos</button>
+      
+      <UploadImagem 
+        setImagem={setImagem} 
+        imagemPreview={imagem?.preview}
+        aoSetarAReferencia={(ref) => referenciaInput.current = ref}
+      />
+      
       <div style={{width: 200}}>
         <Avatar />
         <Botao texto={'Login'} cor ='invertido' manipularClique={() => console.log('botao clicado')} />
